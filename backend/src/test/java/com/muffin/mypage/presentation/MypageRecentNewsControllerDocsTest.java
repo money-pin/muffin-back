6970dd35eb409ddac2b4cd9466173296ef53a763
Eugene-Shin <tdw0312@gmail.com>
2026-08-04 14:55:00 +0900

    [Fix/#140] MypageController 생성자 인자 추가에 따른 기존 테스트 보정
    - 퀴즈 참여 내역 서비스 필드 추가로 늘어난 생성자 인자에 맞춰 기존 3개 테스트의 MypageController 생성 호출 수정

--- backend/src/test/java/com/muffin/mypage/presentation/MypageRecentNewsControllerDocsTest.java
+++ backend/src/test/java/com/muffin/mypage/presentation/MypageRecentNewsControllerDocsTest.java
@@ -155,7 +155,7 @@ class MypageRecentNewsControllerDocsTest {
 
     private MockMvc mockMvcWith(
             MypageRecentNewsQueryService stubService, RestDocumentationContextProvider restDocumentation) {
-        return MockMvcBuilders.standaloneSetup(new MypageController(null, stubService, null))
+        return MockMvcBuilders.standaloneSetup(new MypageController(null, stubService, null, null))
                 .setControllerAdvice(new GeneralExceptionAdvice())
                 .setCustomArgumentResolvers(new AuthenticationPrincipalArgumentResolver())
                 .apply(documentationConfiguration(restDocumentation)
